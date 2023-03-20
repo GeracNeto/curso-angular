e4b27e291f4cd0e3d2b51251515eaef1dbeae292
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2023-03-20 20:50:25 -0300

    Curso Angular 13: Dados no template - #05

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
-  styleUrls: ['./first-component.component.css']
+  styleUrls: ['./first-component.component.css'],
 })
 export class FirstComponentComponent {
-
+  name: string = 'Matheus';
+  age: number = 30;
+  job = 'Programador';
+  hobbies = ['Correr', 'Jogar', 'Nadar'];
+  car = {
+    name: 'Polo',
+    year: 2019,
+  };
 }
