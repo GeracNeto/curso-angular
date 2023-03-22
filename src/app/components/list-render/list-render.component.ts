f60f82f9854db7ede1bd19fa3402418308bd32c0
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2023-03-22 20:07:42 -0300

    Curso Angular 13: A importância das interfaces no Angular - #13

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Animal } from 'src/app/Animal';
 
 @Component({
   selector: 'app-list-render',
@@ -6,22 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-render.component.css'],
 })
 export class ListRenderComponent {
-  animals = [
+  showAnimalDetails: string = '';
+
+  onHandleShowAge(animal: Animal) {
+    this.showAnimalDetails = `O animal ${animal.name} tem ${animal.age} anos`;
+  }
+
+  animals: Animal[] = [
     {
       name: 'Turca',
       type: 'Dog',
+      age: 7,
     },
     {
       name: 'Tom',
       type: 'Cat',
+      age: 5,
     },
     {
       name: 'Frida',
       type: 'Dog',
+      age: 10,
     },
     {
       name: 'Bob',
       type: 'Horse',
+      age: 2,
     },
   ];
 }
