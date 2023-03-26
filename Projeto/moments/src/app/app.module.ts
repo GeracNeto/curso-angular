37d90d41d6412e2318920cbbfe864cd636d6dd51
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2023-03-26 12:00:31 -0300

    PROJETO: Curso Angular 13: Criando interfaces e configurando módulos - #27

--- Projeto/moments/src/app/app.module.ts
+++ Projeto/moments/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -18,13 +21,16 @@ import { MomentFormComponent } from './components/moment-form/moment-form.compon
     HeaderComponent,
     FooterComponent,
     NewMomentComponent,
-    MomentFormComponent
+    MomentFormComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
