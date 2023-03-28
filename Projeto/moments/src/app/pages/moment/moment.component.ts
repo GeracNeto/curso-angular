649076ed18fc9727652d352ac2f0373ca1a8843e
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2023-03-27 21:08:03 -0300

    PROJETO: Curso Angular 13: CSS página de Momentos - #36

--- Projeto/moments/src/app/pages/moment/moment.component.ts
+++ Projeto/moments/src/app/pages/moment/moment.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { faEdit, faTimes } from '@fortawesome/free-solid-svg-icons';
+
 import { Moment } from 'src/app/Moment';
 
 import { MomentService } from 'src/app/services/moment.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-moment',
@@ -11,7 +14,11 @@ import { MomentService } from 'src/app/services/moment.service';
   styleUrls: ['./moment.component.css'],
 })
 export class MomentComponent {
+  baseApiUrl = environment.baseApiUrl;
   moment?: Moment;
+
+  faTimes = faTimes;
+  faEdit = faEdit;
 
   constructor(
     private momentService: MomentService,
